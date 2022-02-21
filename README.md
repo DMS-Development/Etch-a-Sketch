08b876a3b80b4f6dbd9c934e72cb5b48a37572bc
ShimenkoDev <davidshimenko@gmail.com>
2022-02-20 22:06:47 -0700

    Default layout added, ability to change grid of Etch-A-Sketch layout based on user input added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+
+Welcome to Etch-a-Sketch! My third serious project on my web development journey. In this web-app There will be a grid of 'pixels' (size selected by user through a button) that will change color whenever the mouse cursor moves over them!
---
+++ script.js
@@ -0,0 +1,63 @@
+const container = document.querySelector('.gridContainer');
+container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+container.style.gridTemplateRows = 'repeat(16, 1fr)';
+const startButton = document.querySelector('.start');
+
+for (i = 0; i < 256; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.setAttribute('style', 'margin: 0; padding: 0; border: solid black 1px;')
+
+    container.appendChild(cell);
+}
+
+startButton.addEventListener('click', () => {
+    const cellCount = document.querySelectorAll('.cell');
+
+    for (i =0; i < cellCount.length; i++) {
+        container.removeChild(cellCount[i]);
+    }
+
+    let layout;
+    let gridSize;
+
+    do {
+        layout = parseInt(prompt('How many columns and rows would you like to play? Pick between 12 and 100!'));
+        gridSize = layout * layout;
+    } while ((layout < 2 && Number) || (layout > 100 && Number));
+
+    container.style.gridTemplateColumns = `repeat(${layout}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${layout}, 1fr)`;
+
+    for (i = 0; i < gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.setAttribute('style', 'margin: 0; padding: 0; border: solid black 1px;')
+
+        container.appendChild(cell);
+    }
+});
+
+/*function generateCanvas () {
+    const container = document.querySelector('.gridContainer');
+    const cellCount = document.querySelectorAll('.cell');
+
+    for (i =0; i < cellCount.length; i++) {
+        container.removeChild(cellCount[i]);
+    }
+
+    let gridSize = prompt('How many columns and rows would you like to play? Pick between 12 and 100!');
+    gridSize = gridSize * gridSize;
+    let columns = gridSize/gridSize;
+    
+
+
+    for (i = 0; i < gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.setAttribute('style', 'margin: 0; padding: 0; border: solid black 1px;')
+
+        container.appendChild(cell);
+    }
+
+}*/
